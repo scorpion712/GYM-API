6fb4f358c60a6e910aa1fe40ce3f0a1897edce94
Lautaro Diez <lauutii.127@gmail.com>
2024-11-13 16:00:53 +0000

    Get user by id routes and entry point

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express';
 import {
   createUser,
-  getAllUsers
+  getAllUsers,
+  getUserById
 } from '../services/users'; 
 import { CreateUserRequest, userSchema } from '../models';
 import createHttpError from 'http-errors';
@@ -24,6 +25,15 @@ export const getAll = async (req: Request, res: Response) => {
   try {
     const users = await getAllUsers();
     res.status(200).json(users);
+  } catch (error) {
+    res.status(400).send((error as Error).message);
+  }
+};
+
+export const getById = async (req: Request, res: Response) => {
+  try {
+    const user = await getUserById(req.params.id);
+    res.status(200).json(user);
   } catch (error) {
     res.status(400).send((error as Error).message);
   }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'; 
-import { create, getAll } from '../controllers/userController';
+import { create, getAll, getById } from '../controllers/userController';
 
 const router = Router();
 
 router.post('/', create); 
 router.get('/', getAll); 
+router.get('/:id', getById); 
 
 export default router;
