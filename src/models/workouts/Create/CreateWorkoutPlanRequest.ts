ea18b801551cbeb558ddee372a1ff563a2ce6a06
Lautaro Diez <lauutii.127@gmail.com>
2024-11-15 22:45:54 +0000

    Create workout plan models

---
+++ src/models/workouts/Create/CreateWorkoutPlanRequest.ts
@@ -0,0 +1,28 @@
+export type CreateWorkoutPlanRequest = {
+    name: string;
+    objective?: string;
+    duration?: number;
+    initDate: Date;
+    endDate?: Date;
+    workouts: Workout[]; 
+    customersId?: string[];
+} 
+
+type Workout = {
+    id: string;
+    week: number;
+    day: number;
+    description: string;
+    exercises: Exercise[];
+}
+
+type Exercise = {
+    id: string;
+    name: string;
+    series: number;
+    repetitions: number;
+    weight: number;
+    rir: number;
+    rpe: number;
+    comments: string;
+}
