0eca94b1d0e704ba5152b5faddd7a14768bf70b6
Lautaro Diez <lauutii.127@gmail.com>
2024-11-17 18:12:14 +0000

    Get user workout routes and entry point

--- src/routes/workoutRoutes.ts
+++ src/routes/workoutRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'; 
-import { create, edit, getAll, getById } from '../controllers/workoutController';
+import { create, edit, getAll, getById, getUserWorkoutplan } from '../controllers/workoutController';
 
 const router = Router();
 
@@ -7,5 +7,6 @@ router.get('/', getAll);
 router.get('/:id', getById);
 router.post('/', create);
 router.put('/', edit);
+router.get('/User/:id', getUserWorkoutplan);	
 
 export default router;
