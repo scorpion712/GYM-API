0d6b4a4559d77d9b3c759a602d7f63a957fba58d
lautaro.diez@sbi-technology.com <lautaro.diez@sbi-technology.com>
2025-01-23 12:04:52 -0300

    Securize endpoint routes

--- src/routes/workoutRoutes.ts
+++ src/routes/workoutRoutes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express'; 
 import { create, edit, getAll, getById, getUserWorkoutHistory, getUserWorkoutplan } from '../controllers/workoutController';
+import { authMiddleware } from '../middlewares/authMiddleware';
+import { adminRoleMiddleware } from '../middlewares/adminRoleMiddleware';
 
 const router = Router();
 
-router.get('/', getAll);
-router.get('/:id', getById);
-router.post('/', create);
-router.put('/', edit);
+router.use(authMiddleware); 
+
+router.get('/', adminRoleMiddleware, getAll);
+router.get('/:id', adminRoleMiddleware, getById);
+router.post('/', adminRoleMiddleware,create);
+router.put('/', adminRoleMiddleware,edit);
 router.get('/User/:id', getUserWorkoutplan);	
 router.get('/History/User/:id', getUserWorkoutHistory);	
 
